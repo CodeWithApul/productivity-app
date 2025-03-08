d203a6c1f4c90ed3f4cc2bd9ea3516dde1a1c582
Apul Gupta <>
2025-03-08 17:32:50 -0500

    feat: Add user_id column to tasks table with foreign key reference
    - Added user_id column to tasks table
    - Set user_id as a foreign key referencing users.id for relational integrity

--- server/db/queries/tasks.sql
+++ server/db/queries/tasks.sql
@@ -7,3 +7,7 @@ CREATE TABLE
         createdAt timestamp default CURRENT_TIMESTAMP,
         status varchar(20)
     );
+
+ALTER TABLE tasks
+ADD COLUMN user_id INT,
+ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id);
